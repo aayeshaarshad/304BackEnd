769d9ec678bfff8f26b3018828fb0ecba2fc3354
aayeshaarshad <91694678+aayeshaarshad@users.noreply.github.com>
2022-02-12 23:34:24 +0100

    Update waves.js

--- routes/waves.js
+++ routes/waves.js
@@ -2,7 +2,11 @@ const express = require('express'); //import express
 const router  = express.Router(); 
 const waves_controller = require('../controllers/waves');
 
+
+//getting all currencies whom we are going to process or provide moving average and sharpee ratio
 router.get('/getCurrencies', waves_controller.getCurrencies); 
+
+//get wave market prices
 router.get('/getMarkets', waves_controller.getMarkets); 
 
 
