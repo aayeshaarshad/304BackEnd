20c2b932b7c7c045c6efaa8ee25806996e0e8c5e
aayeshaarshad <91694678+aayeshaarshad@users.noreply.github.com>
2022-02-12 23:33:19 +0100

    Update twitterController.js

--- routes/twitterController.js
+++ routes/twitterController.js
@@ -1,5 +1,8 @@
 const express = require('express'); //import express
 const router  = express.Router(); 
 const twitter_controller = require('../controllers/twitterController');
+
+//get the twitter sentiments
 router.get('/getTwitterSentiments', twitter_controller.calculate); 
+
 module.exports = router; // export to use in server.js
