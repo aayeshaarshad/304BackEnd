398e06ee2e19ed08ed77b60373dd3235a52c4927
aayeshaarshad <91694678+aayeshaarshad@users.noreply.github.com>
2022-02-12 23:40:08 +0100

    Update twitterController.js

--- controllers/twitterController.js
+++ controllers/twitterController.js
@@ -1,12 +1,8 @@
+//using twit and sentiment api to read tweets and perform sentiment analysis
+
 const Twit = require('twit');
 const Sentiment = require('sentiment');
-var fs = require('fs');
-const config = require('config');
 
-
-// var tweetJson = {
-//     tweets: []
-//  };
 
 
 var positiveSum=0;
@@ -17,25 +13,23 @@ var totalTweets = 0;
 const sentiment = new Sentiment();
 const T = new Twit({
 
-
+ 
     access_token : "146746630-jFXwDixtdTa5c42FsBfA83Mqq7Ry9B2qK5gnSLJ6",
     access_token_secret : "ZsnJmDV11iNqBxTo12MNKj5LGvb4HyYavq5neZj8RRzer",
     consumer_key : "bY1HGLRjxC1c0pmYsdZtIHHlk",
     consumer_secret : "FBezrcaDRp13JrcQEysyWfai5JMas96dsbDxVj5zABSzS3Fr8r"
-
-    // consumer_key:  config.consumer_key,
-    // consumer_secret: config.consumer_secret,
-    // access_token: req.config.access_token,
-    // access_token_secret: req.config.access_token_secret,
     });
 
 
 var stream = T.stream('statuses/filter', 
 { track: ['waves.tech', 'SignatureChain', 'wavesprotocol', 'neutrino_proto', 'Waves', 'USDN', 'sasha35625' ] })
 
+
+//reading tweet stream
 stream.on('tweet', async tweet => {
     const { lang, text, } = tweet;
 
+   //processing the tweet, and getting the sentiment score
     if (lang === 'en') {
         totalTweets += 1;
         var  score  = sentiment.analyze(text).score;
@@ -47,33 +41,11 @@ stream.on('tweet', async tweet => {
             nuteralSum += 1;
         }
     }
-
-    // tweetJson.tweets.push({"text": text, "score":score});
-    // fs.writeFile('data/tweets.json', JSON.stringify(tweetJson), function(err) {
-    //     if (err) throw err;
-    //     console.log('wrote');
-    // });
-
-
 });
 
 
-
+//returning json with format: {"positive":[POSITIVE_PERCENT],"negative":[NEGATIVE_PERCENT],"neutral":[NEUTRAL_PERCENT],"totalTweetRead":[TOTAL_TWEETS_READ]}
 module.exports.calculate = function(req, res) {
-
-//     var json = JSON.parse(fs.readFileSync('data/tweets.json', 'utf8'));
-//     var positiveSum=0;
-//     var negativeSum=0;
-//     var nuteralSum=0;
-//     var totalTweets = json.tweets.length;
-
-//    for(var i=0; i<totalTweets; i++){
-//         if(json.tweets[i].score == 0){
-
-//         }
-//    }
-
-
     res.json({
         positive: positiveSum/totalTweets*100,
         negative: negativeSum/totalTweets*100,
