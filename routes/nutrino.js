396fabd1729c4b87c32430b6bf00cba2dc085d10
aayeshaarshad <91694678+aayeshaarshad@users.noreply.github.com>
2022-02-12 23:32:54 +0100

    Update nutrino.js

--- routes/nutrino.js
+++ routes/nutrino.js
@@ -2,8 +2,14 @@ const express = require('express'); //import express
 const router  = express.Router(); 
 const nutrino_controller = require('../controllers/nutrino');
 
+
+//get staking rate of specific coin
 router.get('/getRate/:coin', nutrino_controller.getRate); 
+
+//get rate of all neutrino coins
 router.get('/getRate', nutrino_controller.getRates); 
+
+//get the profit - passing argument coin, amount, and time period
 router.get('/getProfit/:coin/:amount/:period', nutrino_controller.getProfit); 
 
 module.exports = router; // export to use in server.js
