62dbd37f294c39bf392e79ba2d374065cac7c41a
aayeshaarshad <91694678+aayeshaarshad@users.noreply.github.com>
2022-02-12 23:31:39 +0100

    Update dataProcess.js
    a

--- routes/dataProcess.js
+++ routes/dataProcess.js
@@ -1,11 +1,23 @@
+
+
 const express = require('express'); //import express
 const router  = express.Router(); 
 const dataProcess_controller = require('../controllers/dataProcess');
 
+
+//get the simple Moving average - passing parameters vs currency and time period
 router.get('/Simple/:vs/:period', dataProcess_controller.calculateSMA); 
+
+//get the Exponential moiving average - passing parameters vs currency and time period
 router.get('/Exponential/:vs/:period', dataProcess_controller.calculateEMA); 
+
+//get the Weighted Moving average - passing parameters vs currency and time period
 router.get('/Weighted/:vs/:period', dataProcess_controller.calculateWMA); 
+
+//get the Simple, Exponential, Weighted Moving averages - passing parameters vs currency and time period
 router.get('/All/:vs/:period', dataProcess_controller.calculateAll); 
+
+//get the sharpee ratio against vs currency, period and risk free rate
 router.get('/SharpeeRatio/:vs/:period/:rfr', dataProcess_controller.calculateSharpeeRatio);
 
 module.exports = router; // export to use in server.js
